3c6a02584f2b4219b8c9c0fa1b089a595980d414
Vasic2000 <Vasic2000@yandex.ru>
2020-03-22 19:07:55 +0300

    BNA Activity

--- app/src/main/java/jp/vasic2000/bottomna/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/jp/vasic2000/bottomna/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Empty Fragment/nNo photo here..."
     }
     val text: LiveData<String> = _text
 }
---
+++ app/src/main/java/jp/vasic2000/bottomna/ui/trains/TrainFragment.kt
@@ -0,0 +1,36 @@
+package jp.vasic2000.bottomna.ui.trains
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import jp.vasic2000.bottomna.R
+import jp.vasic2000.bottomna.ui.trains.DataTrain
+import jp.vasic2000.bottomna.ui.trains.TrainAdapter
+
+class TrainFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.fragment_train, container, false)
+        val recyclerView: RecyclerView = view.findViewById(R.id.recycler_view)
+
+        val layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+//        val layoutManager = GridLayoutManager(context, 2);
+        recyclerView.layoutManager = layoutManager
+        val data = DataTrain()
+        val myAdapter =
+            TrainAdapter(data.getDataList())
+        recyclerView.adapter = myAdapter
+        return view
+    }
+}
